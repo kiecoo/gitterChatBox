6d140ea4ae4b2daec1794f636ed43b4fc411814d
Michelle <34873832+kiecoo@users.noreply.github.com>
2018-03-06 12:09:13 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+function makeGitterURL (username) {
+  return `https://gitter.im/${username}/~embed`
+}
+
+
+var exampleProfile = {
+  name: 'nina',
+  username: 'ninabreznik',
+  photo:'https://nomadlist.com/assets/img/cities/phuket-thailand-500px.jpg'
+}
